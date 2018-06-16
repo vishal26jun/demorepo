70dd7377887aa33873b061ca4e741cde7b8ac184
Vishal <vishal26jun@gmail.com>
2018-06-16 11:58:55 -0700

    this is first commit for demo

---
+++ src/main/java/com/qa/libs/TestBase.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class TestBase {
+
+	public void testBase() {
+		System.out.println("test base class");
+	}
+}
---
+++ src/test/java/com/qa/test/TestLoginPage.java
@@ -0,0 +1,12 @@
+package com.qa.test;
+
+import org.junit.Test;
+
+public class TestLoginPage {
+
+@Test
+public void test1()
+{
+	System.out.println("Testone");
+}
+}
